451109c58bf9dfcb3e43949f3a9860ac5e8b8a7a
kanyukp <petekenyuk@yahoo.com>
2025-04-02 13:13:30 -0700

    Spring project initialized

---
+++ src/main/java/WizardMoneyGroup/MMORPGServer/MmorpgServerApplication.java
@@ -0,0 +1,13 @@
+package WizardMoneyGroup.MMORPGServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MmorpgServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MmorpgServerApplication.class, args);
+	}
+
+}
